001201723036779e3e0e0abbe271ababdbbb6583
Álvaro Rodríguez <alvarorgtr@gmail.com>
2014-08-12 10:09:42 +0200

    [auth] added filtering: redirect user to login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -253,4 +253,9 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  # Adds a custom warden to redirect
+  config.warden do |manager|
+    manager.failure_app = CustomFailure
+  end
 end
