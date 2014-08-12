038cb54f10d58ff4e9666e7ce336d1b88cab4b46
Álvaro Rodríguez <alvarorgtr@gmail.com>
2014-08-12 11:30:50 +0000

    Added username also in forms. Basic rank view

---
+++ app/controllers/devise_registrations_controller.rb
@@ -0,0 +1,12 @@
+class DeviseRegistrationsController < Devise::RegistrationsController
+	# Override the superclass's methods to add custom params
+  private
+ 
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+ 
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
+end
