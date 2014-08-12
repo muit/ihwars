0a7ff69561f7b884f4321186ce49a99698427443
muit <miguel_3c@hotmail.com>
2014-08-12 10:02:22 +0200

    [Update] Staring with views design

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,9 @@
+class BaseController < ApplicationController
+  def index
+    if mobile_device?
+      render :mobile, layout: "mobile"
+    else
+      render :desktop, layout: "desktop"
+    end
+  end
+end
