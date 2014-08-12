63f3c13002e7d3d15f095e87baf42f6004432ae7
muit <miguel_3c@hotmail.com>
2014-08-12 16:25:56 +0200

    Base creation Method

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -9,4 +9,15 @@ class BaseController < ApplicationController
       render :desktop, layout: "desktop"
     end
   end
+
+  def createBase
+    if (current_user.bases.length < Config.maxBases)
+      current_user.bases.create(params[:name]);
+      answerObject = {error: false, msg: ""}
+    else
+      answerObject = {error: true, msg: "Maximum bases reached."}
+    end
+    render :json => Packet.new(Opcode.BASE_CREATE, answerObject)
+  end
+
 end
