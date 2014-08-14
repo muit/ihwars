08a99bb21a5bdb566c3387ff989dd93e6cdda32f
muit <miguel_3c@hotmail.com>
2014-08-14 10:46:42 +0200

    [Fix] Anti Crash when selected cache entities

--- lib/cache.rb
+++ lib/cache.rb
@@ -7,9 +7,11 @@ class Cache
       @entities = Entity.all.map{|entity| entity.attributes}
     end
     def selectEntity(argument, value)
+      return nil if !@entities
       @entities.select{|entity| entity[argument] == value}[0]
     end
     def entity(type_id)
+      return nil if !@entities
       @entities.select{|entity| entity["type_id"] == type_id}[0]
     end
 
@@ -19,9 +21,11 @@ class Cache
       @buildings = Building.all.map{|building| building.attributes}
     end
     def selectBuilding(argument, value)
+      return nil if !@buildings
       @buildings.select{|building| building[argument] == value}[0]
     end
     def building(type_id)
+      return nil if !@buildings
       @buildings.select{|building| building["type_id"] == type_id}[0]
     end
   end
