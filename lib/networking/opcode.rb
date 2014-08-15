432831609594319e7c7c5cd560df8c602964ca50
muit <miguel_3c@hotmail.com>
2014-08-16 00:20:29 +0200

    Added Opcodes

--- lib/networking/opcode.rb
+++ lib/networking/opcode.rb
@@ -1,13 +1,19 @@
 class Opcode
   class << self
     attr_accessor :BASE_CREATE, 
-      :BASE_DELETE, :BUILDING_CREATE, :BUILDING_DELETE
+      :BASE_DELETE, 
+      :BUILDING_CREATE, 
+      :BUILDING_DELETE,
+      :BASE_INFO,
+      :BUILDING_INFO
 
     def load
       @BASE_CREATE = 0x07
       @BASE_DELETE = 0x08
       @BUILDING_CREATE = 0x09
       @BUILDING_DELETE = 0x0a
+      @BASE_INFO = 0x0b
+      @BUILDING_INFO = 0x0c
     end
   end
 end
