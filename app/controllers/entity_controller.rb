40d946ff6875fcc67c4c7b8d98896a209ccb5440
Javier Acero <j4cegu@gmail.com>
2014-08-19 15:33:56 +0200

    EntityController#create action cleanup

--- app/controllers/entity_controller.rb
+++ app/controllers/entity_controller.rb
@@ -1,20 +1,16 @@
 class EntityController < ApplicationController
   def create
-    amount = params[:amount]
     base = params[:selectedBase]
-    type_id = params[:type_id]
     opcode = Opcode::ENTITY_CREATE
 
-    return if watch_error opcode, !hasFactories?, "Can´t create entities without factories!"
+    return_error opcode, "Can't create entities without factories!" unless hasFactories?
+    return_error opcode, "Not enought space in the base. Build houses!" if is_entity_size_exceded?(amount, base)
 
-    return if watch_error opcode, is_entity_size_exceded?(amount, base), "Not enought space in the base. Build houses!"
-
-    stack = base.entity_stacks.where(type_id: type_id).first
-    stack.amount += amount
+    stack = base.entity_stacks.where(type_id: params[:type_id]).first
+    stack.amount += params[:amount]
     stack.save
 
-    answerObject = {error: false, amount: stack.amount}
-    render :json => Packet.new(opcode, answerObject)
+    return_success opcode, amount: stack.amount
   end
 
   def remove
@@ -22,5 +18,13 @@ class EntityController < ApplicationController
   end
 
   private
-  
+
+  def return_success(opcode, answer)
+    render :json => Packet.new(opcode, {error: false}.merge!(answer))
+  end
+
+  def return_error(opcode, message)
+    render :json => Packet.new(opcode, {error: true, msg: message})
+  end
+
 end
