6b94fedcea49617a4f23b915ef8a8c725bc0726f
muit <miguel_3c@hotmail.com>
2014-08-19 15:24:30 +0200

    lol v.2

--- app/controllers/entity_controller.rb
+++ app/controllers/entity_controller.rb
@@ -5,14 +5,14 @@ class EntityController < ApplicationController
     type_id = params[:type_id]
     opcode = Opcode::ENTITY_CREATE
 
-    return if watchError opcode, !hasFactories?, "Can´t create entities without factories!"
+    return if watch_error opcode, !hasFactories?, "Can´t create entities without factories!"
 
-    return if watchError opcode, is_entity_size_exceded?(amount, base), "Not enought space in the base. Build houses!"
+    return if watch_error opcode, is_entity_size_exceded?(amount, base), "Not enought space in the base. Build houses!"
 
     stack = base.entity_stacks.where(type_id: type_id).first
     stack.amount += amount
     stack.save
-    
+
     answerObject = {error: false, amount: stack.amount}
     render :json => Packet.new(opcode, answerObject)
   end
