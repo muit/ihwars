39af5ce21d38522541204e1dbc2d8b266ba34c52
Álvaro Rodríguez <alvarorgtr@gmail.com>
2014-08-19 17:29:43 +0200

    Combat simulator tested

--- app/models/user_rank.rb
+++ app/models/user_rank.rb
@@ -5,11 +5,7 @@ class UserRank < ActiveRecord::Base
 		users = User.all
 		users.each do |a_user|
 			userRank = UserRank.find_or_create_by(user: a_user)
-			userRank.reset
-			a_user.bases.each do |a_base|
-				userRank.add_resource_stacks(a_base.resource_stacks) # This is not useful anymore because now the ranking is by level
-
-			end
+			userRank.set_properties
 			userRank.save
 		end
 	end
@@ -18,23 +14,25 @@ class UserRank < ActiveRecord::Base
 		self.total_resources = 0
 		self.total_materials = 0
 		self.total_money = 0
+		self.total_level = 0
 	end
 
 	def add_resource_stacks(resource_stacks)
+
 		self.total_money += resource_stacks[0].amount
 		self.total_materials += resource_stacks[1].amount
 		self.total_resources += total_money + total_materials
 	end
 
 	def add_level_of_base(base)
-		
+		self.total_level += base.get_level_points
 	end
 
 	def set_properties
 		reset
-		a_user.bases.each do |a_base|
-				userRank.add_resource_stacks(a_base.resource_stacks) # This is not useful anymore because now the ranking is by level
-				userRank.add_level_of_base(a_base)
-			end
+		user.bases.each do |a_base|
+			add_resource_stacks(a_base.resource_stacks) # This is not useful anymore because now the ranking is by level
+			add_level_of_base(a_base)
+		end
 	end
 end
