a72fd67ca5f42c0aaf468428fcc0f4163818ece5
muit <miguel_3c@hotmail.com>
2014-08-19 17:05:42 +0200

    Revert "Heroku TryFix"
    This reverts commit 78967455b912d3e499ec415617210f939048db4e.

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Zaphir
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.assets.initialize_on_precompile = false
+
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
 
